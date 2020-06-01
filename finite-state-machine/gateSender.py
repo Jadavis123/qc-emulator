79cb09b167cb336e4d2584f495fac942b463f911
Jadavis123 <52720468+Jadavis123@users.noreply.github.com>
2020-06-01 16:38:14 -0400

    Communication code for finite state machine

---
+++ finite-state-machine/gateSender.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  1 09:43:33 2020
+
+@author: jacks
+"""
+
+import serial
+import qutip as qt
+from qutip.qip.operations import snot
+from qutip.qip.operations import gate_expand_1toN
+from numToByte import numToByte
+from byteToNum import byteToNum
+from measureTest import measure
+
+ser = serial.Serial('COM4', 115200)
+numQ = 2 #number of qubits
+measureQ = 1 #index of qubit being measured
+count = 0
+outState = qt.basis(2**numQ) - qt.basis(2**numQ) #empty state of appropriate size
+stateNotNorm = qt.basis(2**numQ, 0) + qt.basis(2**numQ, 2)
+state = stateNotNorm.unit() #normalized input state
+gate = gate_expand_1toN(snot(), numQ, 1) #testing gate - currently I x H
+for i in range(2**numQ): #write each element of input state to serial as 8-bit fixed pt
+    probAmp = state.__getitem__(i)[0][0]
+    ser.write(numToByte(probAmp.real))
+    ser.write(numToByte(probAmp.imag))
+for j in range(2**numQ): #write each element of input gate to serial as 8-bit fixed pt
+    rowArray = gate.__getitem__(j)
+    row = rowArray[0]
+    for num in row:
+        ser.write(numToByte(num.real))
+        ser.write(numToByte(num.imag))
+while (count < 2**numQ): #read each element of output state and convert to float, then put into outState
+    num = byteToNum(ser.readline().rstrip().lstrip()[0])
+    outState += num*qt.basis(2**numQ, count)
+    count+=1
+print(measure(outState, measureQ, 2**numQ)) #measure the chosen qubit and print the output
+
+ser.close()
---
+++ finite-state-machine/helloworld.c
@@ -0,0 +1,110 @@
+/******************************************************************************
+*
+* Copyright (C) 2009 - 2014 Xilinx, Inc.  All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* Use of the Software is limited solely to applications:
+* (a) running on a Xilinx device, or
+* (b) that interact with a Xilinx device through a bus or interconnect.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+* XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+* Except as contained in this notice, the name of the Xilinx shall not be used
+* in advertising or otherwise to promote the sale, use or other dealings in
+* this Software without prior written authorization from Xilinx.
+*
+******************************************************************************/
+
+/*
+ * helloworld.c: simple test application
+ *
+ * This application configures UART 16550 to baud rate 9600.
+ * PS7 UART (Zynq) is not initialized by this application, since
+ * bootrom/bsp configures it to baud rate 115200
+ *
+ * ------------------------------------------------
+ * | UART TYPE   BAUD RATE                        |
+ * ------------------------------------------------
+ *   uartns550   9600
+ *   uartlite    Configurable only in HW design
+ *   ps7_uart    115200 (configured by bootrom/bsp)
+ */
+
+#include <stdio.h>
+#include "platform.h"
+#include "xil_printf.h"
+#include "xparameters.h"
+#include "xiomodule.h"
+
+//Quantum Computer Finite State Machine
+
+volatile char int_flag = 0;
+void InterruptFlagSet(void* ref){
+  int_flag = 1;
+}
+
+int main()
+{
+ init_platform();
+
+ int count = 0;
+ int numQ = 2;
+ int numStates = (1 << numQ);
+ int numInput = numStates * (numStates + 1);
+ u32 data;
+ XIOModule iomodule;
+ u8 rx_buf[10];
+
+ data = XIOModule_Initialize(&iomodule, XPAR_IOMODULE_0_DEVICE_ID);
+ data = XIOModule_Start(&iomodule);
+
+ microblaze_register_handler(XIOModule_DeviceInterruptHandler, XPAR_IOMODULE_0_DEVICE_ID);
+     XIOModule_Connect(&iomodule, XIN_IOMODULE_GPI_2_INTERRUPT_INTR, InterruptFlagSet, NULL); // register timerTick() as our interrupt handler
+     XIOModule_Enable(&iomodule, XIN_IOMODULE_GPI_2_INTERRUPT_INTR);
+     microblaze_enable_interrupts();
+
+ while(count < numInput)
+ {
+	 while (XIOModule_Recv(&iomodule, rx_buf, 1) == 0);
+	 XIOModule_DiscreteWrite(&iomodule, 1, (u32) rx_buf[0]);
+	 XIOModule_DiscreteWrite(&iomodule, 2, 1); //set the output flag high
+	 while (XIOModule_Recv(&iomodule, rx_buf, 1) == 0);
+	 XIOModule_DiscreteWrite(&iomodule, 1, (u32) rx_buf[0]);
+	 XIOModule_DiscreteWrite(&iomodule, 2, 0); //set the output flag low
+	 count++;
+ }
+
+ while(1)
+ {
+	while(int_flag == 0);
+	while(count < numStates)
+	{
+		data = XIOModule_DiscreteRead(&iomodule, 1);
+		xil_printf("%c\n\r", (u8) data);
+		XIOModule_DiscreteWrite(&iomodule, 2, 1);
+		data = XIOModule_DiscreteRead(&iomodule, 1);
+		xil_printf("%c\n\r", (u8) data);
+		XIOModule_DiscreteWrite(&iomodule, 2, 0);
+		count++;
+	}
+ }
+
+ cleanup_platform();
+ return 0;
+}
+
